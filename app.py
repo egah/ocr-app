5bcc72cf620fd9c8961a63cb23d71c1650f5def9
egah <egahepiphane@gmail.com>
2023-12-27 17:07:21 +0100

    delete abby ocr for credantial reason

--- app.py
+++ app.py
@@ -1,83 +1,79 @@
-import os
 from PIL import Image
 
 import streamlit as st
 
-from utils import (
-    tesseract_ocr,
-    abbyy_ocr,
-)
-
-st.set_page_config(
-    page_title="OCR Engine",
-    layout="centered",
-    initial_sidebar_state="expanded",
-    page_icon=":mag:",
-)
-
-st.title("Optical Character Recognition")
-
-# Main title
-# st.sidebar.markdown(
-#    "<center> <img src='picture.jpg' width='200'> </center>",
-#    unsafe_allow_html=True,
-# )
-
-st.sidebar.image("picture.jpg", width=200, use_column_width=False)
-
-# with open("description.html", mode="r") as description:
-#    html_file = description.read()
-# st.sidebar.markdown(html_file, unsafe_allow_html=True)
-st.sidebar.caption("# Epiphane Egah 🤗️🚀️\n")
-st.sidebar.info(
-    """Analytics Enginee : Python - NLP - Statistics - Machine learning -
-    Deep learning  - DevOps - MLops"""
-)
-
-# Settings
-
-# OCR = st.multiselect(
-#    "Select OCR engine",
-#    ("Tesseract", "Abbyy")
-# )
-st.markdown(
-    "<b><h3 style='color: #77B5FE;'>Select OCR engine</h4></b>", unsafe_allow_html=True
-)
-OCR = st.selectbox(" ", ("Tesseract", "Abbyy"))
-path = "data"
-
-st.markdown(
-    "<b><h3 style='color: #FF0000;'>Upload a file</h4></b>", unsafe_allow_html=True
-)
-uploaded_file = st.file_uploader("", type=["jpg", "png", "jpeg"])
-
-if uploaded_file is not None:
-    # Use the file
-    # st.markdown("**file to OCR**")
-    uploaded_image = Image.open(uploaded_file)
-    # Use the image
-    # st.image(uploaded_image, width=1000, use_column_width=True)
+from utils import tesseract_ocr
 
 
-if OCR == "Tesseract" and uploaded_file is not None:
-    ocr_result = tesseract_ocr(uploaded_image, "eng")
-    with open("ocr_result.txt", "w", encoding="utf-8") as file:
-        file.write(ocr_result)
-    st.download_button(
-        label="Download OCR Result",
-        data=open("ocr_result.txt", "r", encoding="utf-8").read(),
-        file_name="ocr_result.txt",
-        mime="text/plain",
+def main():
+    st.set_page_config(
+        page_title="OCR Engine",
+        layout="centered",
+        initial_sidebar_state="expanded",
+        page_icon=":mag:",
     )
 
-# TODO add an api key for AbbyyOCR and allowed user to use abbyoce to
-if OCR == "Abbyy" and uploaded_file is not None:
-    pass
+    st.title("Optical Character Recognition")
 
-# Other info
-st.sidebar.title("Description")
-st.sidebar.info("This basic app can be used to ocr file with tesseracct and AbbyyOCR. ")
-st.sidebar.title("Disclaimer")
-st.sidebar.info(
-    "For any questions or further details regarding techniques used, please contact egahepiphane@gmail.com"
-)
+    # Main title
+    # st.sidebar.markdown(
+    #    "<center> <img src='picture.jpg' width='200'> </center>",
+    #    unsafe_allow_html=True,
+    # )
+
+    st.sidebar.image("picture.jpg", width=200, use_column_width=False)
+
+    # with open("description.html", mode="r") as description:
+    #    html_file = description.read()
+    # st.sidebar.markdown(html_file, unsafe_allow_html=True)
+    st.sidebar.caption("# Epiphane Egah 🤗️🚀️\n")
+    st.sidebar.info(
+        """Analytics Enginee : Python - NLP - Statistics - Machine learning -
+        Deep learning  - DevOps - MLops"""
+    )
+
+    # Settings
+
+    # OCR = st.multiselect(
+    #    "Select OCR engine",
+    #    ("Tesseract", "Abbyy")
+    # )
+
+    # OCR = st.selectbox(" ", ("Tesseract", "Abbyy"))
+
+    st.markdown(
+        "<b><h3 style='color: #FF0000;'>Upload a file</h4></b>", unsafe_allow_html=True
+    )
+    uploaded_file = st.file_uploader("", type=["jpg", "png", "jpeg"])
+
+    if uploaded_file is not None:
+        # Use the file
+        # st.markdown("**file to OCR**")
+        uploaded_image = Image.open(uploaded_file)
+        # Use the image
+        # st.image(uploaded_image, width=1000, use_column_width=True)
+
+    if uploaded_file is not None:
+        ocr_result = tesseract_ocr(uploaded_image, "eng")
+        with open("ocr_result.txt", "w", encoding="utf-8") as file:
+            file.write(ocr_result)
+        st.download_button(
+            label="Download OCR Result",
+            data=open("ocr_result.txt", "r", encoding="utf-8").read(),
+            file_name="ocr_result.txt",
+            mime="text/plain",
+        )
+
+    # Other info
+    st.sidebar.title("Description")
+    st.sidebar.info(
+        "This basic app can be used to ocr file with tesseracct and AbbyyOCR. "
+    )
+    st.sidebar.title("Disclaimer")
+    st.sidebar.info(
+        "For any questions or further details regarding techniques used, please contact egahepiphane@gmail.com"
+    )
+
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -1,9 +1,4 @@
-from scipy import spatial
-from ABBYY import CloudOCR
-import numpy as np
-import cv2
 import pytesseract
-from decimal import Decimal
 
 pytesseract.pytesseract.tesseract_cmd = "tesseract"
 
@@ -12,45 +7,3 @@ def tesseract_ocr(image_to_ocr, lang):
     custom_config = r"--oem 1"
     text = pytesseract.image_to_string(image_to_ocr, lang=lang, config=custom_config)
     return text
-
-
-def abbyy_ocr(jpg_file, lang):
-    ocr_engine = CloudOCR(
-        application_id="5766ee3d-45f0-4cf9-88c9-f51721847735",
-        password="Su/SO91YzA2xjxCS30aAnL3P",
-    )
-    jpg = open(jpg_file, "rb")
-    file = {jpg.name: jpg_file}
-    result = ocr_engine.process_and_download(file, exportFormat="txt", language=lang)
-    file = result.get("txt")
-    return file.getvalue().decode("utf-8")
-
-
-def bag_of_words(verite_terrain, ocr_result):
-    bag_ref = verite_terrain.split()
-    bag_ocr = ocr_result.split()
-    error = 0
-    for elem in np.unique(bag_ref):
-        error = error - bag_ocr.count(elem) + bag_ref.count(elem)
-    acc = (len(bag_ref) - error) * 100 / len(bag_ref)
-    acc_deci = Decimal(str(round(acc, 2)))
-    accuracy = float(acc_deci)
-    return accuracy
-
-
-def cosine_similarity(verite_terrain, ocr_result):
-    bag_ref = verite_terrain.split()
-    bag_ocr = ocr_result.split()
-    ref_elem = []
-    doc_elem = []
-    for elem in np.unique(bag_ref):
-        ref_elem.append(bag_ocr.count(elem))
-        doc_elem.append(bag_ref.count(elem))
-    result = 1 - spatial.distance.cosine(ref_elem, doc_elem)
-    result_deci = Decimal(str(round(result, 2)))
-    cosine_sim = float(result_deci)
-    return cosine_sim
-
-
-if __name__ == "__main__":
-    pass
