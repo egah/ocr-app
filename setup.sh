96452cda194993b60fe7a589c23da84ddd9d4082
Epiphane EGAH <egahepiphane@gmail.com>
2024-01-04 23:54:51 +0100

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+[general]\n\
+email = \egahepiphane@gmail.com"\n\
+\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
