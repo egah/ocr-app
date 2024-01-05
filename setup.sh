92093dbc6a17bec70668afe09c3e607fc6f59ba9
Epiphane EGAH <egahepiphane@gmail.com>
2024-01-05 06:12:25 +0100

    add conf

--- setup.sh
+++ setup.sh
@@ -2,14 +2,12 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \egahepiphane@gmail.com"\n\
-\n\
+email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
 " > ~/.streamlit/config.toml
